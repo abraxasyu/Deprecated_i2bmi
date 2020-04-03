1c7ff345f61b434e416249e82b4b1dbf00383453
bganglia <bganglia892@gmail.com>
2020-04-02 21:28:46 -0400

    rst fixes

--- docs/index.rst
+++ docs/index.rst
@@ -3,18 +3,5 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to i2bmi's documentation!
-=================================
-
-.. toctree::
-   :maxdepth: 2
-   :caption: Contents:
-
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+.. automodule:: i2bmi
+   :members:
--- i2bmi/__init__.py
+++ i2bmi/__init__.py
@@ -1,3 +1,18 @@
+"""
+I2BMI
+=====
+
+Biomedical Informatics toolkit by Institute for Informatics at Washington University School of Medicine in St. Louis
+
+This package provides tools for converting medical data between different formats. Here is a diagram showing which function to use, based on the source and destination formats:
+
+.. image:: ../Format_Diagram.jpg
+
+API Guide
+---------
+"""
+
+
 import pandas as pd
 import numpy as np
 import os
